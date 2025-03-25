4bde84873fbe8fffd9f0c7c8c65c033ddfb98ad5
canal-clarke <23103439@usc.edu.ph>
2025-03-25 16:07:52 +0800

    Add project files.

---
+++ To do part 3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace To_do_part_3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ To do part 3/ToDoPage.xaml.cs
@@ -0,0 +1,13 @@
+
+namespace To_do_part_3;
+
+public partial class ToDoPage : ContentPage
+{
+	public ToDoPage()
+	{
+		InitializeComponent();
+	}
+
+
+
+}
